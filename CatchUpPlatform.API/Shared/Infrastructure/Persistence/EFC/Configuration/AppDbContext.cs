8e2ce3f0d7f3b171a0d811aa812ac8d683b054a3
MarceloMazuelos <u201916143@upc.edu.pe>
2025-10-24 18:30:25 -0500

    Added in AppDbContext.cs News Bounded Context Object-Relational Mapping Configurations

--- CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,3 +1,4 @@
+using CatchUpPlatform.API.News.Domain.Model.Aggregates;
 using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
@@ -6,17 +7,27 @@ namespace CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuratio
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
-        optionsBuilder.AddCreatedUpdatedInterceptor();
-        base.OnConfiguring(optionsBuilder);
+        builder.AddCreatedUpdatedInterceptor();
+        base.OnConfiguring(builder);
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
         
-        // Apply Case to Database Object
+        // News Bounded Context Object-Relational Mapping Configurations
+        builder.Entity<FavoriteSource>().HasKey(f => f.Id);
+        builder.Entity<FavoriteSource>().Property(f => f.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<FavoriteSource>().Property(f => f.NewsApiKey).IsRequired().HasMaxLength(100);
+        builder.Entity<FavoriteSource>().Property(f => f.SourceId).IsRequired().HasMaxLength(100);
+        builder.Entity<FavoriteSource>().HasIndex(f => new { f.NewsApiKey, f.SourceId }).IsUnique();
+        
+        
+        
+        
+        // Apply snake_case naming convention to database objects
         builder.UseSnakeCaseNamingConvention();
     }
 }
